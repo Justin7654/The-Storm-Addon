e4e5042f0086543d727aeb91e5453518627ce4e6
Justin Olsen <justinolsen2007@gmail.com>
2023-12-09 14:00:48 -0500

    Added enum obj table and basic season event system

--- script.lua
+++ script.lua
@@ -29,6 +29,7 @@ g_savedata = {
         WIND_AMOUNT = property.slider("Storm Wind Amount (%)", 50, 150, 10, 120)/100, --The peak wind intensity of a storm
         FOG_AMOUNT = property.slider("Storm Fog Amount (%)", 50, 100, 10, 80)/100, --The peak fog intensity of a storm
         POWER_FAILURE_CHANCE = property.slider("Power failure chance every second (%)", 1,5, 0.1, 1), --The chance that a vehicle will fail during a storm
+        BYPASS_SEASONAL_EVENTS = false,
     },
     storm = {
         ["active"] = false,
@@ -42,15 +43,9 @@ g_savedata = {
             fog = nil,
         },
     },
-    powerFailures = {
-        
-    },
-    playerVehicles = {
-
-    },
-    playerMoodStates = {
-
-    },
+    powerFailures = {},
+    playerVehicles = {},
+    playerMoodStates = {},
 }
 
 settingConversionData = {
@@ -66,8 +61,76 @@ settingConversionData = {
     WIND_AMOUNT = "number",
     FOG_AMOUNT = "number",
     POWER_FAILURE_CHANCE = "number",
+    BYPASS_SEASONAL_EVENTS = "bool",
 }
 
+enum = {
+    SEASONAL_EVENTS = {
+        NONE = 0,
+        HALLOWEEN = 1,
+        CHRISTMAS = 2,
+    },
+    NOTIFICATION_TYPES = {
+        NEW_MISSION = 0,
+        NEW_MISSION_CRITICAL = 1,
+        FAILED_MISSION = 2,
+        FAILED_MISSION_CRITICAL = 3,
+        COMPLETE_MISSION = 4,
+        NETWORK_CONNECT = 5,
+        NETWORK_DISCONNECT = 6,
+        NETWORK_INFO = 7,
+        CHAT_MESSAGE = 8,
+        NETWORK_INFO_CRITICAL = 9,
+    },
+    MAP_OBJECT = {
+        POSITION_TYPES = {
+            FIXED = 0,
+            VEHICLE = 1,
+            OBJECT = 2,
+        },
+        MARKER_TYPES = {
+            DELIVERY_TARGET = 0,
+            SURVIVOR = 1,
+            OBJECT = 2,
+            WAYPOINT = 3,
+            TUTORIAL = 4,
+            FIRE = 5,
+            SHARK = 6,
+            ICE = 7,
+            SEARCH_RADIUS = 8,
+            FLAG_1 = 9,
+            FLAG_2 = 10,
+            HOUSE = 11,
+            CAR = 12,
+            PLANE = 13,
+            TANK = 14,
+            HELI = 15,
+            SHIP = 16,
+            BOAT = 17,
+            ATTACK = 18,
+            DEFEND = 19,
+        },
+    },
+    MAP_LABEL = {
+        LABEL_TYPES = {
+            NONE = 0,
+            CROSS = 1,
+            WRECKAGE = 2,
+            TERMINAL = 3,
+            MILITARY = 4,
+            HERITAGE = 5,
+            RIG = 6,
+            INDUSTRIAL = 7,
+            HOSPITAL = 8,
+            SCIENCE = 9,
+            AIRPORT = 10,
+            COASTGUARD = 11,
+            LIGHTHOUSE = 12,
+            FUEL = 13,
+            FUEL_SELL = 14,
+        },
+    },
+}
 
 function onCreate(is_world_create)
     if g_savedata.settings.MIN_LENGTH > g_savedata.settings.MAX_LENGTH then g_savedata.settings.MIN_LENGTH = g_savedata.settings.MAX_LENGTH end
@@ -426,7 +489,14 @@ end
 --- Starts the storm
 function startStorm()
     printDebug("(startStorm) called", true)
-    server.notify(-1, "Broadcast", "A storm is on the horizon.", 4)
+    season = server.getSeasonalEvent()
+    if season == enum.SEASONAL_EVENTS.CHRISTMAS then
+        server.notify(-1, "Broadcast", "A blizzard is on the horizon.", 4)
+    elseif season == enum.SEASONAL_EVENTS.HALLOWEEN then
+        server.notify(-1, "Broadcast", "Something is forming on the horizon...", 4)
+    else
+        server.notify(-1, "Broadcast", "A storm is on the horizon.", 4)
+    end
     g_savedata.storm.active = true
     g_savedata.storm.stage = "windUp"
     g_savedata.storm["tweenStart"] = g_savedata.tick_counter
@@ -439,7 +509,14 @@ end
 function endStorm()
     printDebug("(endStorm) called", true)
     if storm.active == false then return end
-    server.notify(-1, "Broadcast", "The storm seems to be clearing.", 4)
+    season = server.getSeasonalEvent()
+    if season == enum.SEASONAL_EVENTS.EVENTS.CHRISTMAS then
+        server.notify(-1, "Broadcast", "The blizzard seems to be clearing.", 4)
+    elseif season == enum.SEASONAL_EVENTS.HALLOWEEN then
+        server.notify(-1, "Broadcast", "The storm seems to be clearing.", 4)
+    else
+        server.notify(-1, "Broadcast", "It seems to be clearing up...", 4)
+    end
     storm = g_savedata.storm
     storm.stage = "windDown";
     g_savedata.storm["tweenStart"] = g_savedata.tick_counter
