2e2c4a87cd59a44e237f69fe233dfef1cf2140c2
Justin Olsen <justinolsen2007@gmail.com>
2023-12-30 14:59:03 -0500

    Checking to see if gitPull actually worked, and removed the enum library due to limitations with github autopilot

--- libs/util.lua
+++ libs/util.lua
@@ -1,5 +1,8 @@
 util = {}
 
+---@param tags table The list of tags to search through
+---@param tag string the tag to check for
+---@return boolean isPresent Weather or not the tag is present in the list 
 function util.hasTag(tags, tag)
     for i in pairs(tags) do
         if tags[i] == tag then
